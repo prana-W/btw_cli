63ddac335cf406ba984c545244428ca3d91b9579
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2025-08-02 01:56:23 +0530

    Display attendance data in tabular format

--- commands/attendance.js
+++ commands/attendance.js
@@ -1,6 +1,20 @@
 import sapStore from '../config/sapStore.js';
 import chalk from 'chalk';
 import attendanceData from '../lib/getAttendanceData.js';
+import CliTable3 from 'cli-table3';
+import { oraPromise } from 'ora';
+
+const table = new CliTable3({
+    head: [
+        'Subject Code',
+        'Subject Name',
+        'Faculty Name',
+        'Total Classes',
+        'Attendance(%)',
+    ]
+});
+
+const userDataTable = new CliTable3();
 
 export default async function attendance() {
     try {
@@ -20,12 +34,34 @@ export default async function attendance() {
             (sapStore.get('lastUpdated') - Date.now()) / (1000 * 60 * 60); // in hours
 
         if (timeDifference < 24) {
-            // display the saved data
+            //todo: display the saved data
             return;
         }
 
         // Fetch the Attendance Data again from the website
 
-        const response = await attendanceData(username, password);
-    } catch (err) {}
+        const response = await oraPromise(attendanceData(username, password), 'Fetching Attendance Data...');
+
+        userDataTable.push(
+            {
+                'Roll Number': response[0].roll,
+            },
+            { Date: response[0].date },
+            { 'Current Time': response[0].current_time },
+        );
+        console.log(userDataTable.toString());
+
+
+        response.shift();
+
+        response.forEach((row) => {
+            const rowData = Object.values(row);
+            table.push(rowData);
+        });
+
+        console.log(table.toString());
+
+    } catch (err) {
+        console.error(chalk.red(err?.message || err));
+    }
 }
